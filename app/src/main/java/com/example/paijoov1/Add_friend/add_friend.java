89207ad435ab4c27b8d06a3cbb51bafeca97eb7e
thanut <thanutza_dmex@gmail.com>
2019-11-17 03:47:00 +0700

    Added add friend page class + xml and put temporary images for all the temporary friends in home fragment

---
+++ app/src/main/java/com/example/paijoov1/Add_friend/add_friend.java
@@ -0,0 +1,15 @@
+package com.example.paijoov1.Add_friend;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.paijoov1.R;
+
+public class add_friend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+}
